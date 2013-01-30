9d2f4e51d78ddc7498d96f4aa18631921a36fb90
Paulo Patto <paulopatto@gmail.com>
2013-01-30 17:59:37 -0200

    Initial commit

---
+++ seed.js
@@ -0,0 +1,28 @@
+var places = [
+    {
+        name: "Parque do Ibirapuera",
+        loc:[-23.583143, -46.658077]
+    },
+    {
+        name: "MASP",
+        loc[-23.560761, -46.655974]
+    },
+    {
+        name: "Catedral da Se",
+        loc:[-23.561438, -46.655969]
+    },
+    {
+        name: "Aeroporto do Galeao",
+        loc: [-22.814440, -43.236265]
+    },
+    {
+
+        name: "Marques de Sapucai",
+        loc: [-22.911875, -43.193221]
+    }
+];
+var mongo = new Mongo();
+for(place in places){
+
+
+}
